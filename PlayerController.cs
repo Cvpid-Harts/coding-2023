1c800b352f8003e0371538e6a7f6cee07baf3e2f
Cvpid-Harts <144938240+Cvpid-Harts@users.noreply.github.com>
2023-11-08 12:05:28 -0500

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,27 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
 {
 
- 
+    public float speed = 10.0f;
+    public float horizontalInput;
+    public float verticalInput;
+    public float xRange = 10.0f;
+    public float zRange = 10.0f;
+    public GameObject projectilePrefab; 
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    private float turnSpeed = 45.0f;
-    private float horizontalInput;
-    private float forwardInput;
-    private float speed = 20.0f;
+
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        // mmove vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        { 
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
+
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        { 
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+    
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
