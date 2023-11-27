45b3b95fc8bdcb222c1fbaca334a58fdbfa5f07e
Cvpid-Harts <144938240+Cvpid-Harts@users.noreply.github.com>
2023-11-27 12:29:13 -0500

    Add files via upload

--- PlayerControllerX.cs
+++ PlayerControllerX.cs
@@ -4,31 +4,15 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-
-    }
-    
-    private float speed = 20.0f;
-    private float rotationSpeed = 45.0f;
-    private float verticalInput;
-    private float forwardInput;
+    public GameObject dogPrefab;
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
-        forwardInput = Input.GetAxis("Horizontal");
-
-        // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-
-        // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right, rotationSpeed * verticalInput * Time.deltaTime);
-
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
     }
 }
