a25d7f72a07d9c1a688b7c043b9a8e376fccf412
Cvpid-Harts <144938240+Cvpid-Harts@users.noreply.github.com>
2024-01-12 09:47:09 -0500

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,62 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour;
+private Rigidbody playerRb;
+public float jumpForce;
+public float gravityModifer;
+public GameObject obstaclePrefab;
+private Vector3 spawnPos = new Vector3(25, 0, 0);
+private float startDelay = 2;
+private float repeatRate = 2;
 
 {
-
-    public float speed = 10.0f;
-    public float horizontalInput;
-    public float verticalInput;
-    public float xRange = 10.0f;
-    public float zRange = 10.0f;
-    public GameObject projectilePrefab; 
-
-    // Start is called before the first frame update
+// Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
-
-
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-
-
-
-        if (transform.position.z < -zRange)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
-        }
-
-        if (transform.position.z > zRange)
-        { 
-            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
-        }
-
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
-
-
-        if (transform.position.z < -zRange)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
-        }
-
-        if (transform.position.z > zRange)
-        { 
-            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
-    
-        }
-
-        if(Input.GetKeyDown(KeyCode.Space))
-
-        {
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
+        playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
     }
 }
