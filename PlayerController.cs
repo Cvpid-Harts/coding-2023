17da7089e4a441472dc9293e03ba9df05fb93060
Cvpid-Harts <144938240+Cvpid-Harts@users.noreply.github.com>
2024-01-31 08:53:44 -0500

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,31 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour;
-private Rigidbody playerRb;
-public float jumpForce;
-public float gravityModifer;
-public GameObject obstaclePrefab;
-private Vector3 spawnPos = new Vector3(25, 0, 0);
-private float startDelay = 2;
-private float repeatRate = 2;
-
+public class PlayerController : MonoBehaviour
 {
-// Start is called before the first frame update
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+
+    // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
-        playerRB = GetComponent<Rigidbody>();
-        Physics.gravity *= gravityModifier;
-    }
-    void SpawnObstacle()
-    {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround)
+        {
+            playerRb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+     
+    {
+        isOnGround = true;
     }
 }
