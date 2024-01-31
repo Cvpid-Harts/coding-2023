c7b0a510bd9268c7dfcbfcc4e8466b7aad248645
Cvpid-Harts <144938240+Cvpid-Harts@users.noreply.github.com>
2024-01-31 10:56:15 -0500

    Add files via upload

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +29,7 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision collision)
+     
     {
         isOnGround = true;
     }
--- Scripts/SpawnManager.cs
+++ Scripts/SpawnManager.cs
@@ -6,10 +6,12 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    public float startDelay = 2;
+    public float repeatRate = 5;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate)
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
         
     }
 
