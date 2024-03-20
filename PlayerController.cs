8148dbc33a647c11a68cc8ecd3a2b47d8c4c815c
Cvpid-Harts <144938240+Cvpid-Harts@users.noreply.github.com>
2024-03-20 12:22:50 -0400

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,52 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 8;
+    public float forwardInput;
     private Rigidbody playerRb;
-    public float jumpForce;
-    public float gravityModifier;
-    public bool isOnGround = true;
-    public bool gameOver = false;
-    private Animator playerAnim;
-    public ParticleSystem explostionParticle;
-    public ParticleSystem dirtParticle;
-
+    private GameObject focalPoint;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        Physics.gravity *= gravityModifier;
-        playerAnim = GetComponent<Animator>(); 
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround)
-        {
-            playerRb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
-            isOnGround = false;
-            playerAnim.SetTrigger("Jump_trig");
-            dirtParticle.Stop();
-        }
+        forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        
     }
-
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("Ground")) {
-            isOnGround = true;
-            dirtParticle.Play();
-
-        }
-        else if (collision.gameObject.CompareTag("Obstacle")) {
-            gameOver = true;
-            Debug.Log("Game over!");
-            playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType_int", 1);
-            explostionParticle.Play();
-            dirtParticle.Stop();
-
-        }
-
-    }
-     
 }
